98eb0bdccd68814381c45a4d716fca21f8a2bab8
Steven Zelek <11831150+smzelek@users.noreply.github.com>
2023-08-03 17:48:50 -0400

    chore: setup deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <span style={{ display: "none", }}>
+        {process.env['COMMIT_HASH']}
+      </span>
       <Planner />
     </>
   )
